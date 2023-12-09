926853a326ed7fc85d48a8f9bd67d4ba9d6b7f44
pigpgw <ceh20002@naver.com>
2023-12-09 12:52:27 +0900

    feat Nested Routes 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import data from './data'
-import { Routes, Route, Link  } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate  } from 'react-router-dom'
 import Detail from './pages/detail';
 import Card from './components/Card';
 
 function App() {
 
   let [shoes, setShoes] = useState(data);
+  let navigate =  useNavigate();
 
   return (
     <div className="App">
@@ -17,11 +18,9 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link to={"/"}>홈</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
-            <Nav.Link href="#pricing">Shoes</Nav.Link>
-      
-            <Link to={"/detail"}>상세 페이지</Link>
+            <Nav.Link onClick={(() => { navigate('/') })}>홈</Nav.Link>
+            <Nav.Link onClick={(() => { navigate('/detail') })}>Detail</Nav.Link>
+            {/* <Nav.Link onClick={(() => { navigate(-1) })}>뒤로가기</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
@@ -41,16 +40,35 @@ function App() {
               </div>
             </div>
           </>
-        }> </Route>
-        <Route path='/detail' element={
-          <>
-            <Detail />
-          </>
-        } />
-        <Route path='about' element={<div>어바웃 페이지임</div>} />
+        }> 
+        </Route>
+
+        <Route path='/detail' element={<><Detail/></>} />
+        <Route path='*' element={<div>없는 페이지</div>} />
+
+        {/* Nested Routes */}
+        <Route path='/about' element={<About />} >
+          <Route path='member' element={<About />} />
+          <Route path='location' element={<About />} />
+        </Route >
+
+        {/* <Route path='/about' element={<></>} />
+        <Route path='/about/member' element={<></>} />
+        <Route path='/about/loc' element={<></>} /> */}
+
+        
       </Routes>
     </div>
   );
+
+    function About(){
+      return (
+        <div>
+          <h4>회사정보</h4>
+        </div>
+      )
+    }
+
 }
 
 export default App;
