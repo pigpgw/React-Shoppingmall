76fa3735eca8ec60b8cde89e18c4468aee636a63
pigpgw <ceh20002@naver.com>
2023-12-09 16:13:08 +0900

    feat 상세 페이지 아이템 id로 뿌려주기 (메인에서 신발들을 이름순이나 다른 것으로 정렬시 데이터가 바뀌는 문제를 해결하기 위해서)

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,4 +1,14 @@
-export default function Detail() {
+import { useParams } from "react-router-dom"
+
+export default function Detail(props) {
+
+    let {id} = useParams();
+
+    console.log("id",id)
+
+    let findItem = props.shoes.filter((item) => item.id === Number(id))[0];
+
+
     return (
         <div className="container">
             <div className="row">
@@ -6,9 +16,10 @@ export default function Detail() {
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
+                    <h4 className="pt-5">{findItem.title}</h4>
+                    <div>{findItem.id}</div>
+                    <p>{findItem.content}</p>
+                    <p>{findItem.price}원</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
